1352638d020428b1fdce84694d553fa5b2ba95a3
yerim <yerim4327@gmail.com>
2024-12-08 22:50:15 +0900

    게임 설명창 구현(게임화면1-2)/게임 시작 버튼

--- src/assets.py
+++ src/assets.py
@@ -3,16 +3,16 @@ import pygame
 
 # 에셋 경로
 assets_paths = {
-    "close" : "C:/Users/user/Desktop/TALA/TALA/assets/Icon/close.png",
-    "soundON" : "C:/Users/user/Desktop/TALA/TALA/assets/Icon/soundON.png",
-    "soundOFF" : "C:/Users/user/Desktop/TALA/TALA/assets/Icon/soundOFF.png",
-    "bigScreen": "C:/Users/user/Desktop/TALA/TALA/assets/Icon/bigScreen.png",
-    "smallScreen": "C:/Users/user/Desktop/TALA/TALA/assets/Icon/smallScreen.png",
-    "stampBoard": "C:/Users/user/Desktop/TALA/TALA/assets/Icon/stampBoard.png",
-    "home" : "C:/Users/user/Desktop/TALA/TALA/assets/Icon/home.png",
-    "voice" : "C:/Users/user/Desktop/TALA/TALA/assets/Icon/voiceSupport.png",
-    "font" : "C:/Users/user/Desktop/TALA/TALA/assets/fonts/Ramche.ttf",
-    "bgm" : "C:/Users/user/Desktop/TALA/TALA/assets/Bgm/sound-k-117217.mp3"
+    "close" : "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/close.png",
+    "soundON" : "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/soundON.png",
+    "soundOFF" : "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/soundOFF.png",
+    "bigScreen": "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/bigScreen.png",
+    "smallScreen": "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/smallScreen.png",
+    "stampBoard": "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/stampBoard.png",
+    "home" : "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/home.png",
+    "voice" : "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Icon/voiceSupport.png",
+    "font" : "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/fonts/Ramche.ttf",
+    "bgm" : "C:/Users/shrew/OneDrive/바탕 화면/t/team1/assets/Bgm/sound-k-117217.mp3"
 }
 
 # 이미지 로딩 및 크기 조정 함수
